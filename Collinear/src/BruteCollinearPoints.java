ddf8525489178c70ea53c9aebdb016afa2ef7d1e
julienfumanti <julien.fumanti@gmail.com>
2019-09-24 22:35:48 +0200

    Collinear v1

--- Collinear/src/BruteCollinearPoints.java
+++ Collinear/src/BruteCollinearPoints.java
@@ -1,17 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BruteCollinearPoints {
-  // finds all line segments containing 4 points
-  public BruteCollinearPoints(Point[] points) {
+    private ArrayList<LineSegment> jSegments = new ArrayList<>();
 
-  }
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
 
-  // the number of line segments
-  public int numberOfSegments() {
-    return 0;
-  }
+        Point[] jCopy = points.clone();
+        Arrays.sort(jCopy);
 
-  // the line segments
-  public LineSegment[] segments() {
-    return new LineSegment[2];
-  }
+        if (hasDuplicate(jCopy)) {
+            throw new IllegalArgumentException("U have duplicate points");
+        }
+
+        for (int i = 0; i < jCopy.length - 3; i++) {
+            for (int j = i + 1; j < jCopy.length - 2; j++) {
+                double slopeFS = jCopy[i].slopeTo(jCopy[j]);
+                for (int third = j + 1; third < jCopy.length - 1; third++) {
+                    double slopeFT = jCopy[i].slopeTo(jCopy[third]);
+                    if (slopeFS == slopeFT) {
+                        for (int forth = third + 1; forth < jCopy.length; forth++) {
+                            double slopeFF = jCopy[i].slopeTo(jCopy[forth]);
+                            if (slopeFS == slopeFF) {
+                                jSegments.add(new LineSegment(jCopy[i], jCopy[forth]));
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    private boolean hasDuplicate(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return jSegments.size();
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return jSegments.toArray(new LineSegment[jSegments.size()]);
+    }
 
 }
--- Collinear/src/FastCollinearPoints.java
+++ Collinear/src/FastCollinearPoints.java
@@ -1,16 +1,64 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FastCollinearPoints {
-  // finds all line segments containing 4 or more points
-  public FastCollinearPoints(Point[] points) {
+    private ArrayList<LineSegment> jSegments = new ArrayList<>();
 
-  }
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException();
+        }
+        Point[] jCopy = points.clone();
+        Arrays.sort(jCopy);
 
-  // the number of line segments
-  public int numberOfSegments() {
-    return 0;
-  }
+        if (hasDuplicate(jCopy)) {
+            throw new IllegalArgumentException("U have duplicate points");
+        }
 
-  // the line segments
-  public LineSegment[] segments() {
-    return new LineSegment[2];
-  }
+        for (int i = 0; i < jCopy.length - 3; i++) {
+            Arrays.sort(jCopy);
+
+            // Sort the points according to the slopes they makes with p.
+            // Check if any 3 (or more) adjacent points in the sorted order
+            // have equal slopes with respect to p. If so, these points,
+            // together with p, are collinear.
+
+            Arrays.sort(jCopy, jCopy[i].slopeOrder());
+
+            for (int p = 0, first = 1, last = 2; last < jCopy.length; last++) {
+                // find last collinear to p point
+                while (last < jCopy.length
+                        && Double.compare(jCopy[p].slopeTo(jCopy[first]), jCopy[p].slopeTo(jCopy[last])) == 0) {
+                    last++;
+                }
+                // if found at least 3 elements, make segment if it's unique
+                if (last - first >= 3 && jCopy[p].compareTo(jCopy[first]) < 0) {
+                    jSegments.add(new LineSegment(jCopy[p], jCopy[last - 1]));
+                }
+                // Try to find next
+                first = last;
+            }
+        }
+    }
+
+    // test the whole array fo duplicate points
+    private boolean hasDuplicate(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            if (points[i].compareTo(points[i + 1]) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return 0;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+        return jSegments.toArray(new LineSegment[jSegments.size()]);
+    }
 }
