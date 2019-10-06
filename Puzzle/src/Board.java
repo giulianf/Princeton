0ae03a215c22693321a221264c4a1e3253c68569
julienfumanti <julien.fumanti@gmail.com>
2019-10-06 19:32:48 +0200

    Puzzle v2 62/100

--- Puzzle/src/Board.java
+++ Puzzle/src/Board.java
@@ -1,24 +1,38 @@
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdRandom;
 
-import static java.lang.Math.abs;
+import java.util.Arrays;
 
 public class Board {
-    private static final int DIRKA = 0;
+    private final int n;
 
-    private int jBoardDimension;
-    private char[] jBoard;
+    private final char[] tiles;
+    private int blankpos;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        jBoardDimension = tiles.length;
-        jBoard = new char[jBoardDimension * jBoardDimension];
-        int elem = 0;
-        for (int[] cs : tiles) {
-            for (int c : cs) {
-                jBoard[elem++] = (char) c;
+        n = tiles.length;
+        this.tiles = new char[n * n];
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                this.tiles[k] = (char) tiles[i][j];
+                if (tiles[i][j] == 0) blankpos = k;
+                k++;
             }
         }
+    }
+
+    // first row = 1
+    private int row(int p) {
+        return (int) Math.ceil((double) p / (double) n);
+    }
+
+    // first column = 1
+    private int col(int p) {
+        if (p % n == 0) return n;
+        return p % n;
     }
 
     // unit testing (not graded)
@@ -28,179 +42,181 @@ public class Board {
 
     // string representation of this board
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(jBoardDimension);
-        sb.append("\n");
-        int rest = 0;
-        for (int i = 0; i < jBoard.length; i++) {
-            sb.append(String.format("%2d ", (int) jBoard[i]));
-            // alws make new line followed by matrix dimension
-            if (rest++ % jBoardDimension == jBoardDimension - 1) {
-                sb.append("\n");
+        StringBuilder s = new StringBuilder();
+        s.append(n);
+        s.append("\n");
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", (int) tiles[k]));
+                k++;
             }
+            s.append("\n");
         }
-        return sb.toString();
+        return s.toString();
     }
 
     // board dimension n
     public int dimension() {
-        return jBoardDimension;
+        return n;
     }
 
     // number of tiles out of place
     public int hamming() {
-        int out = 0;
-        int i = 1;
-        // from 1 to end
-        for (char c : jBoard) {
-            if ((int) c != 0 && (int) c != i) {
-                // check last properly
-                if ((int) c == 0 && (int) c == jBoard.length - 1) {
-                    continue;
-                }
-                out++;
-            }
-            i++;
+        int hamming = 0;
+        for (int k = 0, ans = 1; k < n * n; k++, ans++) {
+            if (tiles[k] == 0) continue;
+            if (tiles[k] != ans) hamming++;
         }
-        return out;
+        return hamming;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        int out = 0;
-        int i = 1;
-        // from 1 to end
-        for (char c : jBoard) {
-            if ((int) c != 0 && (int) c != i) {
-                // check last properly
-                if ((int) c == 0 && (int) c == jBoard.length - 1) {
-                    continue;
-                }
-                // row steps
-                final int dR = toXY((int) c)[0] - toXY(i)[0];
-                // colomn
-                final int dC = toXY((int) c)[1] - toXY(i)[1];
-                // count all
-                out += abs(dR) + abs(dC);
-            }
-            i++;
+        int manhattan = 0;
+        for (int k = 0; k < n * n; k++) {
+            if (tiles[k] == 0) continue;
+            int rowdiff = Math.abs(row(tiles[k]) - row(k + 1));
+            int coldiff = Math.abs(col(tiles[k]) - col(k + 1));
+            manhattan = manhattan + rowdiff + coldiff;
         }
-        return out;
+        return manhattan;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        return hamming() == 0;
-    }
-
-    // does this board equal y?
-    public boolean equals(Object y) {
-        if (y == null)
-            return false;
-        if (y == this)
-            return true;
-        if (y.getClass() != this.getClass())
-            return false;
-        Board that = (Board) y;
-        if (that.dimension() != this.dimension())
-            return false;
-        for (int i = 0; i < that.dimension() * that.dimension(); i++) {
-            if (this.jBoard[i] != that.jBoard[i])
-                return false;
+        for (int k = 0; k < n * n - 2; k++) {
+            if (tiles[k] > tiles[k + 1]) return false;
         }
         return true;
     }
 
-    // all neighboring boards
-    public Iterable<Board> neighbors() {
-        Queue<Board> jNeighbors = new Queue<Board>();
-        int currentDIRKAIndex = -1;
-        // try to find dirka
-        for (int i = 0; i < jBoard.length; i++) {
-            if (jBoard[i] == DIRKA) {
-                currentDIRKAIndex = i;
-                break;
-            }
-        }
-        int n = currentDIRKAIndex / jBoardDimension;
-        int m = currentDIRKAIndex % jBoardDimension;
-        // dirka moves UP
-        if (n - 1 >= 0) {
-            Board u = smallTwin(false);
-            u.swapTiles(currentDIRKAIndex, xyTo1D(n - 1, m));
-            jNeighbors.enqueue(u);
-        }
-        // dirka moves down
-        if (n + 1 < jBoardDimension) {
-            Board d = smallTwin(false);
-            d.swapTiles(currentDIRKAIndex, xyTo1D(n + 1, m));
-            jNeighbors.enqueue(d);
-        }
-        // dirka moves left
-        if (m - 1 >= 0) {
-            Board l = smallTwin(false);
-            l.swapTiles(currentDIRKAIndex, xyTo1D(n, m - 1));
-            jNeighbors.enqueue(l);
-        }
-        // dirka moves right
-        if (m + 1 < jBoardDimension) {
-            Board r = smallTwin(false);
-            r.swapTiles(currentDIRKAIndex, xyTo1D(n, m + 1));
-            jNeighbors.enqueue(r);
-        }
-        return jNeighbors;
+    // does this board equal y?
+    public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        Board that = (Board) y;
+        if (!Arrays.equals(this.tiles, that.tiles)) return false;
+        return true;
     }
+
+    public Iterable<Board> neighbors() {
+        Stack<Board> stackNeighbors = new Stack<Board>();
+        char[] neighbor;
+        if (row(blankpos + 1) != 1) {
+            neighbor = tiles.clone();
+            swapAbove(neighbor, blankpos);
+            Board neighborBoard = new Board(toTwoDarray(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if (row(blankpos + 1) != n) {
+            neighbor = tiles.clone();
+            swapBelow(neighbor, blankpos);
+            Board neighborBoard = new Board(toTwoDarray(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if (col(blankpos + 1) != 1) {
+            neighbor = tiles.clone();
+            swapLeft(neighbor, blankpos);
+            Board neighborBoard = new Board(toTwoDarray(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if (col(blankpos + 1) != n) {
+            neighbor = tiles.clone();
+            swapRight(neighbor, blankpos);
+            Board neighborBoard = new Board(toTwoDarray(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        return stackNeighbors;
+    }
+
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return smallTwin(true);
-    }
-
-    // a board that is obtained by exchanging any pair of tiles
-    private Board smallTwin(boolean swap) {
-        boolean twined = false;
-        // clone 1D array into 2D
-        int[][] twinBoard = new int[jBoardDimension][jBoardDimension];
-        int jIndex = 0;
-        int iOne = 0, iTwo = 0;
-        for (int i = 0; i < jBoardDimension; i++) {
-            for (int k = 0; k < jBoardDimension; k++) {
-                twinBoard[i][k] = this.jBoard[jIndex++];
-                // find first two tiles (not DIRKA) and remember to swap
-                if (!twined && jIndex > 0 && (int) this.jBoard[jIndex - 1] != DIRKA
-                        && (int) this.jBoard[jIndex] != DIRKA) {
-                    iOne = jIndex;
-                    iTwo = jIndex - 1;
-                    twined = true;
-                }
+        boolean swapSuccess = false;
+        char[] twin = tiles.clone();
+        // choose a non-blank block
+        int k = 0;
+        do {
+            k = StdRandom.uniform(n * n);
+        } while (tiles[k] == 0);
+        // choose an exchange direction
+        while (swapSuccess == false) {
+            int choice = StdRandom.uniform(4);
+            switch (choice) {
+                case 0: //swapAbove
+                    if (row(k + 1) == 1) swapSuccess = false;
+                    else if (twin[k - n] == 0) swapSuccess = false;
+                    else {
+                        swapAbove(twin, k);
+                        swapSuccess = true;
+                    }
+                    break;
+                case 1: //swapBelow
+                    if (row(k + 1) == n) swapSuccess = false;
+                    else if (twin[k + n] == 0) swapSuccess = false;
+                    else {
+                        swapBelow(twin, k);
+                        swapSuccess = true;
+                    }
+                    break;
+                case 2: //swapLeft
+                    if (col(k + 1) == 1) swapSuccess = false;
+                    else if (twin[k - 1] == 0) swapSuccess = false;
+                    else {
+                        swapLeft(twin, k);
+                        swapSuccess = true;
+                    }
+                    break;
+                case 3: //swapRight
+                    if (col(k + 1) == n) swapSuccess = false;
+                    else if (twin[k + 1] == 0) swapSuccess = false;
+                    else {
+                        swapRight(twin, k);
+                        swapSuccess = true;
+                    }
+                    break;
             }
         }
-        Board twin = new Board(twinBoard);
-
-        if (swap) {
-            twin.swapTiles(iOne, iTwo);
-        }
-        return twin;
+        return new Board(toTwoDarray(twin));
     }
 
-    // returns [position] in 1D array from [X][Y] position of 2D array
-    private int xyTo1D(final int x, final int y) {
-        return (jBoardDimension * (x) + y);
+    //swap functions
+    private void swapAbove(char[] oneDarray, int k) {
+        char temp = oneDarray[k];
+        oneDarray[k] = oneDarray[k - n];
+        oneDarray[k - n] = temp;
     }
 
-    // returns {X,Y} coords from [1D] coordinate.
-    // init array count begins from 1 !!!
-    private int[] toXY(final int d) {
-        int x = (d - 1) / jBoardDimension;
-        int y = (d - 1) % jBoardDimension;
-        return new int[]{x, y};
+    private void swapBelow(char[] oneDarray, int k) {
+        char temp = oneDarray[k];
+        oneDarray[k] = oneDarray[k + n];
+        oneDarray[k + n] = temp;
     }
 
-    // swap tiles i and k of this.jBoard
-    private void swapTiles(int i, int k) {
-        char aux = this.jBoard[i];
-        this.jBoard[i] = this.jBoard[k];
-        this.jBoard[k] = aux;
+    private void swapLeft(char[] oneDarray, int k) {
+        char temp = oneDarray[k];
+        oneDarray[k] = oneDarray[k - 1];
+        oneDarray[k - 1] = temp;
     }
+
+    private void swapRight(char[] oneDarray, int k) {
+        char temp = oneDarray[k];
+        oneDarray[k] = oneDarray[k + 1];
+        oneDarray[k + 1] = temp;
+    }
+
+    private int[][] toTwoDarray(char[] oneDarray) {
+        int k = 0;
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = oneDarray[k];
+                k++;
+            }
+        return tiles;
+    }
+
 
 }
