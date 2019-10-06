347093c090400c873800de595d8acc264af4783d
julienfumanti <julien.fumanti@gmail.com>
2019-10-06 20:05:34 +0200

    Puzzle v3 62/100 timing 100%

--- Puzzle/src/Solver.java
+++ Puzzle/src/Solver.java
@@ -2,50 +2,57 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
 
 public class Solver {
-    private JNode bNode, bTwinNode;
+    private MinPQ<SearchNode> pq;
+    private MinPQ<SearchNode> pqTwin;
+    private int n;
+    private Board initial;
+    private Board goal;
+    private SearchNode end;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
-        int moves = 0;
-        bNode = new JNode(initial, moves);
-        bTwinNode = new JNode(initial.twin(), moves);
-        MinPQ<JNode> jQueue = new MinPQ<>();
+        //varify input
+        if (initial == null) throw new NullPointerException();
 
-        //insert both
-        jQueue.insert(bNode);
-        jQueue.insert(bTwinNode);
-        //choose nice one to solve
-        bNode = jQueue.delMin();
+        this.initial = initial;
+        n = initial.dimension();
+        pq = new MinPQ<>();
+        pqTwin = new MinPQ<>();
 
-        // till the very end...
-        while (!bNode.isGoal()) {
-            // make node neighbors stack
-            Stack<Board> neighbors = new Stack<Board>();
-            //and add all the neighbors for this node there
-            for (Board b : bNode.neighbors()) {
-                neighbors.push(b);
+        int[][] blocks = new int[n][n];
+        int k = 1;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+                blocks[i][j] = k;
+                k++;
             }
+        blocks[n - 1][n - 1] = 0;
+        goal = new Board(blocks);
 
-            moves = bNode.moves + 1;
-
-            // now check each neighbor
-            for (Board b : neighbors) {
-                Board bPrevious = null;
-
-                // temp previous state
-                if (bNode.previous != null) {
-                    bPrevious = bNode.previous.getjBoard();
-                }
-                // when find really NEW one,
-                // add new node to Queue
-                if (!b.equals(bPrevious)) {
-                    JNode node = new JNode(b, moves);
-                    node.previous = bNode;
-                    jQueue.insert(node);
+        SearchNode minNode;
+        SearchNode minNodeTwin;
+        pq.insert(new SearchNode(initial, 0, null));
+        pqTwin.insert(new SearchNode(initial.twin(), 0, null));
+        while (!pq.min().board.equals(goal) && !pqTwin.min().board.equals(goal)) {
+            minNode = pq.min();
+            minNodeTwin = pqTwin.min();
+            pq.delMin();
+            pqTwin.delMin();
+            for (Board neighbor : minNode.board.neighbors()) {
+                if (minNode.moves == 0) {
+                    pq.insert(new SearchNode(neighbor, minNode.moves + 1, minNode));
+                } else if (!neighbor.equals(minNode.previousNode.board)) {
+                    pq.insert(new SearchNode(neighbor, minNode.moves + 1, minNode));
                 }
             }
-            // next node to proceed
-            bNode = jQueue.delMin();
+            // Twin
+            for (Board neighbor : minNodeTwin.board.neighbors()) {
+                if (minNodeTwin.moves == 0) {
+                    pqTwin.insert(new SearchNode(neighbor, minNodeTwin.moves + 1, minNodeTwin));
+                } else if (!neighbor.equals(minNodeTwin.previousNode.board)) {
+                    pqTwin.insert(new SearchNode(neighbor, minNodeTwin.moves + 1, minNodeTwin));
+                }
+            }
         }
     }
 
@@ -56,76 +63,51 @@ public class Solver {
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        // get solved
-        JNode firstNode = bNode;
-        // and move backwards to the start
-        while (firstNode.previous != null) {
-            firstNode = firstNode.previous;
+
+        if (pq.min().board.equals(goal)) {
+            return true;
         }
-        // compare init state
-        // if solved one not twin, so exactly initial was solvable haha xD
-        // or vise versa
-        return firstNode.getjBoard().equals(bTwinNode.getjBoard()) ? false : true;
+        if (pqTwin.min().board.equals(goal)) {
+            return false;
+        }
+        return false;
     }
 
     // min number of moves to solve initial board
     public int moves() {
-        return isSolvable() ? bNode.moves : -1;
+        if (!isSolvable()) return -1;
+        //return end.moves;
+        return pq.min().moves;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        JNode sequenceNode = bNode;
-
-        if (isSolvable()) {
-            Stack<Board> solutionQueue = new Stack<Board>();
-
-            solutionQueue.push(sequenceNode.getjBoard());
-            while (sequenceNode.previous != null) {
-                sequenceNode = sequenceNode.previous;
-                solutionQueue.push(sequenceNode.getjBoard());
-            }
-            return solutionQueue;
-        } else
-            return null;
+        if (!isSolvable()) return null;
+        Stack<Board> stackSolution = new Stack<>();
+        SearchNode current = pq.min();
+        while (current.previousNode != null) {
+            stackSolution.push(current.board);
+            current = current.previousNode;
+        }
+        stackSolution.push(initial);
+        return stackSolution;
     }
 
-    private class JNode implements Comparable<JNode> {
-        private JNode previous;
-        private Board jBoard;
+    private class SearchNode implements Comparable<SearchNode> {
+        private Board board;
         private int moves;
+        private int priority;
+        private SearchNode previousNode;
 
-        public JNode(Board jBoard, int moves) {
-            this.jBoard = jBoard;
+        public SearchNode(Board board, int moves, SearchNode previousNode) {
+            this.board = board;
             this.moves = moves;
-            previous = null;
+            priority = moves + board.manhattan();
+            this.previousNode = previousNode;
         }
 
-        public Board getjBoard() {
-            return jBoard;
-        }
-
-        public boolean isGoal() {
-            return jBoard.isGoal();
-        }
-
-        public Iterable<Board> neighbors() {
-            return jBoard.neighbors();
-        }
-
-        // add moves to simple manhattan
-        public int heuristic() {
-            return jBoard.manhattan() + moves;
-        }
-
-        @Override
-        public int compareTo(JNode that) {
-            return this.heuristic() - that.heuristic();
-        }
-
-        @Override
-        public String toString() {
-            return jBoard.toString();
+        public int compareTo(SearchNode that) {
+            return (this.priority - that.priority);
         }
     }
 }
