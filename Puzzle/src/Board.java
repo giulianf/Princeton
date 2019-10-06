ba72f4056d7c51cacb2bfd594139dc70020f8db5
julienfumanti <julien.fumanti@gmail.com>
2019-10-06 20:08:30 +0200

    Puzzle v4 92% Correctness:  49/51 tests passed Memory:       10/22 tests passed Timing:       125/125 tests passed

--- Puzzle/src/Board.java
+++ Puzzle/src/Board.java
@@ -1,222 +1,155 @@
-import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.StdRandom;
-
-import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Board {
-    private final int n;
+    private static final int SPACE = 0;
 
-    private final char[] tiles;
-    private int blankpos;
+    private int[][] tiles;
 
-    // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        n = tiles.length;
-        this.tiles = new char[n * n];
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                this.tiles[k] = (char) tiles[i][j];
-                if (tiles[i][j] == 0) blankpos = k;
-                k++;
-            }
-        }
+        this.tiles = copy(tiles);
     }
 
-    // first row = 1
-    private int row(int p) {
-        return (int) Math.ceil((double) p / (double) n);
+    private int[][] copy(int[][] tiles) {
+        int[][] copy = new int[tiles.length][tiles.length];
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                copy[row][col] = tiles[row][col];
+
+        return copy;
     }
 
-    // first column = 1
-    private int col(int p) {
-        if (p % n == 0) return n;
-        return p % n;
-    }
-
-    // unit testing (not graded)
-    public static void main(String[] args) {
-
-    }
-
-    // string representation of this board
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(n);
-        s.append("\n");
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                s.append(String.format("%2d ", (int) tiles[k]));
-                k++;
-            }
-            s.append("\n");
-        }
-        return s.toString();
-    }
-
-    // board dimension n
     public int dimension() {
-        return n;
+        return tiles.length;
     }
 
-    // number of tiles out of place
     public int hamming() {
-        int hamming = 0;
-        for (int k = 0, ans = 1; k < n * n; k++, ans++) {
-            if (tiles[k] == 0) continue;
-            if (tiles[k] != ans) hamming++;
-        }
-        return hamming;
+        int count = 0;
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                if (blockIsNotInPlace(row, col)) count++;
+
+        return count;
     }
 
-    // sum of Manhattan distances between tiles and goal
+    private boolean blockIsNotInPlace(int row, int col) {
+        int block = block(row, col);
+
+        return !isSpace(block) && block != goalFor(row, col);
+    }
+
+    private int goalFor(int row, int col) {
+        return row * dimension() + col + 1;
+    }
+
+    private boolean isSpace(int block) {
+        return block == SPACE;
+    }
+
     public int manhattan() {
-        int manhattan = 0;
-        for (int k = 0; k < n * n; k++) {
-            if (tiles[k] == 0) continue;
-            int rowdiff = Math.abs(row(tiles[k]) - row(k + 1));
-            int coldiff = Math.abs(col(tiles[k]) - col(k + 1));
-            manhattan = manhattan + rowdiff + coldiff;
-        }
-        return manhattan;
+        int sum = 0;
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                sum += calculateDistances(row, col);
+
+        return sum;
     }
 
-    // is this board the goal board?
+    private int calculateDistances(int row, int col) {
+        int block = block(row, col);
+
+        return (isSpace(block)) ? 0 : Math.abs(row - row(block)) + Math.abs(col - col(block));
+    }
+
+    private int block(int row, int col) {
+        return tiles[row][col];
+    }
+
+    private int row(int block) {
+        return (block - 1) / dimension();
+    }
+
+    private int col(int block) {
+        return (block - 1) % dimension();
+    }
+
     public boolean isGoal() {
-        for (int k = 0; k < n * n - 2; k++) {
-            if (tiles[k] > tiles[k + 1]) return false;
-        }
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                if (blockIsInPlace(row, col)) return false;
+
         return true;
     }
 
-    // does this board equal y?
+    private boolean blockIsInPlace(int row, int col) {
+        int block = block(row, col);
+
+        return !isSpace(block) && block != goalFor(row, col);
+    }
+
+    public Board twin() {
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length - 1; col++)
+                if (!isSpace(block(row, col)) && !isSpace(block(row, col + 1)))
+                    return new Board(swap(row, col, row, col + 1));
+        throw new RuntimeException();
+    }
+
+    private int[][] swap(int row1, int col1, int row2, int col2) {
+        int[][] copy = copy(tiles);
+        int tmp = copy[row1][col1];
+        copy[row1][col1] = copy[row2][col2];
+        copy[row2][col2] = tmp;
+
+        return copy;
+    }
+
     public boolean equals(Object y) {
         if (y == this) return true;
-        if (y == null) return false;
-        if (y.getClass() != this.getClass()) return false;
-        Board that = (Board) y;
-        if (!Arrays.equals(this.tiles, that.tiles)) return false;
+        if (y == null || !(y instanceof Board) || ((Board) y).tiles.length != tiles.length) return false;
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                if (((Board) y).tiles[row][col] != block(row, col)) return false;
+
         return true;
     }
 
     public Iterable<Board> neighbors() {
-        Stack<Board> stackNeighbors = new Stack<Board>();
-        char[] neighbor;
-        if (row(blankpos + 1) != 1) {
-            neighbor = tiles.clone();
-            swapAbove(neighbor, blankpos);
-            Board neighborBoard = new Board(toTwoDarray(neighbor));
-            stackNeighbors.push(neighborBoard);
+        LinkedList<Board> neighbors = new LinkedList<Board>();
+
+        int[] location = spaceLocation();
+        int spaceRow = location[0];
+        int spaceCol = location[1];
+
+        if (spaceRow > 0) neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow - 1, spaceCol)));
+        if (spaceRow < dimension() - 1) neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow + 1, spaceCol)));
+        if (spaceCol > 0) neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol - 1)));
+        if (spaceCol < dimension() - 1) neighbors.add(new Board(swap(spaceRow, spaceCol, spaceRow, spaceCol + 1)));
+
+        return neighbors;
+    }
+
+    private int[] spaceLocation() {
+        for (int row = 0; row < tiles.length; row++)
+            for (int col = 0; col < tiles.length; col++)
+                if (isSpace(block(row, col))) {
+                    int[] location = new int[2];
+                    location[0] = row;
+                    location[1] = col;
+
+                    return location;
+                }
+        throw new RuntimeException();
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append(dimension() + "\n");
+        for (int row = 0; row < tiles.length; row++) {
+            for (int col = 0; col < tiles.length; col++)
+                str.append(String.format("%2d ", block(row, col)));
+            str.append("\n");
         }
-        if (row(blankpos + 1) != n) {
-            neighbor = tiles.clone();
-            swapBelow(neighbor, blankpos);
-            Board neighborBoard = new Board(toTwoDarray(neighbor));
-            stackNeighbors.push(neighborBoard);
-        }
-        if (col(blankpos + 1) != 1) {
-            neighbor = tiles.clone();
-            swapLeft(neighbor, blankpos);
-            Board neighborBoard = new Board(toTwoDarray(neighbor));
-            stackNeighbors.push(neighborBoard);
-        }
-        if (col(blankpos + 1) != n) {
-            neighbor = tiles.clone();
-            swapRight(neighbor, blankpos);
-            Board neighborBoard = new Board(toTwoDarray(neighbor));
-            stackNeighbors.push(neighborBoard);
-        }
-        return stackNeighbors;
+
+        return str.toString();
     }
-
-
-    // a board that is obtained by exchanging any pair of tiles
-    public Board twin() {
-        boolean swapSuccess = false;
-        char[] twin = tiles.clone();
-        // choose a non-blank block
-        int k = 0;
-        do {
-            k = StdRandom.uniform(n * n);
-        } while (tiles[k] == 0);
-        // choose an exchange direction
-        while (swapSuccess == false) {
-            int choice = StdRandom.uniform(4);
-            switch (choice) {
-                case 0: //swapAbove
-                    if (row(k + 1) == 1) swapSuccess = false;
-                    else if (twin[k - n] == 0) swapSuccess = false;
-                    else {
-                        swapAbove(twin, k);
-                        swapSuccess = true;
-                    }
-                    break;
-                case 1: //swapBelow
-                    if (row(k + 1) == n) swapSuccess = false;
-                    else if (twin[k + n] == 0) swapSuccess = false;
-                    else {
-                        swapBelow(twin, k);
-                        swapSuccess = true;
-                    }
-                    break;
-                case 2: //swapLeft
-                    if (col(k + 1) == 1) swapSuccess = false;
-                    else if (twin[k - 1] == 0) swapSuccess = false;
-                    else {
-                        swapLeft(twin, k);
-                        swapSuccess = true;
-                    }
-                    break;
-                case 3: //swapRight
-                    if (col(k + 1) == n) swapSuccess = false;
-                    else if (twin[k + 1] == 0) swapSuccess = false;
-                    else {
-                        swapRight(twin, k);
-                        swapSuccess = true;
-                    }
-                    break;
-            }
-        }
-        return new Board(toTwoDarray(twin));
-    }
-
-    //swap functions
-    private void swapAbove(char[] oneDarray, int k) {
-        char temp = oneDarray[k];
-        oneDarray[k] = oneDarray[k - n];
-        oneDarray[k - n] = temp;
-    }
-
-    private void swapBelow(char[] oneDarray, int k) {
-        char temp = oneDarray[k];
-        oneDarray[k] = oneDarray[k + n];
-        oneDarray[k + n] = temp;
-    }
-
-    private void swapLeft(char[] oneDarray, int k) {
-        char temp = oneDarray[k];
-        oneDarray[k] = oneDarray[k - 1];
-        oneDarray[k - 1] = temp;
-    }
-
-    private void swapRight(char[] oneDarray, int k) {
-        char temp = oneDarray[k];
-        oneDarray[k] = oneDarray[k + 1];
-        oneDarray[k + 1] = temp;
-    }
-
-    private int[][] toTwoDarray(char[] oneDarray) {
-        int k = 0;
-        int[][] tiles = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++) {
-                tiles[i][j] = oneDarray[k];
-                k++;
-            }
-        return tiles;
-    }
-
-
 }
